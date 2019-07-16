ab22557ac98a43646673c64cc9517a2bfbd8254f
Olli Rousu <olli.rousu@hotmail.com>
2019-07-16 12:00:26 +0300

    part 2 ex 6-10 done

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already in the phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+
+      <h2>Filter the phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+
+      <h2>Add a new number</h2>
+      <PersonForm addNumber={addNumber}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons filteredPersons={filteredPersons}/>
+    </div>
+  )
+
+}
+
+export default App
---
+++ osa2/puhelinluettelo/src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Filter = ({filter, handleFilterChange}) => {
+  return (
+    <div>
+      filter: <input value={filter}
+        onChange={handleFilterChange}
+      />
+    </div>
+  )
+}
+
+export default Filter
