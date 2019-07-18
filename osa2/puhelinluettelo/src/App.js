1f162c414d75f0530c78ba1bd05e7e59aeac3cae
Olli Rousu <olli.rousu@hotmail.com>
2019-07-18 12:03:10 +0300

    part2 ex 19-20 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,12 +3,14 @@ import personService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     personService.getAll()
@@ -31,12 +33,20 @@ const App = () => {
           .then(res => {
             setPersons(persons.map(person => person.id !== personId.id ? person : res))
           })
+        setMessage(`Henkilön ${newName} numero päivitetty`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       }
     } else {
       personService.create(personObject)
         .then(res => {
           setPersons(persons.concat(res))
         })
+      setMessage(`Henkilö ${newName} lisätty puhelinluetteloon`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
     setNewName('')
     setNewNumber('')
@@ -60,6 +70,17 @@ const App = () => {
         .then(res => {
           setPersons(persons.filter(person => person.id !== id))
         })
+        .catch(error => {
+          setMessage('Käyttäjän tiedot on jo poistettu')
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+          setPersons(persons.filter(person => person.id !== id))
+        })
+      setMessage(`Poisto onnistui`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
   }
 
@@ -67,6 +88,9 @@ const App = () => {
 
   return (
     <div>
+
+      <Notification message={message} />
+
       <h1>Phonebook</h1>
 
       <h2>Filter the phonebook</h2>
