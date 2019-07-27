ba77e82be75e7370b53cd2e3db0404a88ab1472f
Olli Rousu <olli.rousu@hotmail.com>
2019-07-27 23:05:01 +0300

    4.2 done

---
+++ osa4/blogs/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to database')
+  })
+  .catch(error => {
+    console.log('Error while connecting to database', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
