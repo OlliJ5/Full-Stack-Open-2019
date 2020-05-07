a9f5b0a8ca2b117cfcfc19c4e11e3820ab65ec4c
Olli Rousu <olli.rousu@hotmail.com>
2020-05-07 13:28:16 +0300

    9.14 done

---
+++ osa9/courses/src/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+interface CourseProps {
+  name: string;
+  exerciseCount: number;
+}
+
+const Content: React.FC<{ courseParts: CourseProps[] }> = ({ courseParts }) => {
+  return (
+    <div>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </div>
+  )
+}
+
+const Total: React.FC<{ courseParts: CourseProps[] }> = ({ courseParts }) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  )
+}
+
+const App: React.FC = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
